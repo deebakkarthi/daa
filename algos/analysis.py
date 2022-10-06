2c67b4253f5af6665b6f2b8b54912990a522f658
Deebakkarthi C R <dbk@deebakkarthi.com>
2022-10-06 13:07:24 +0530

    Initial plots

--- algos/analysis.py
+++ algos/analysis.py
@@ -38,24 +38,25 @@ if __name__ == "__main__":
     match sys.argv[1]:
         case "q":
             func = partial(qsort, start=0, end=None)
-            sys.stdout = open("../data/quicksort", 'w')
+            sys.stdout = open("../data/quicksort.csv", 'w')
         case "m":
             func = msort
-            sys.stdout = open("../data/mergesort", 'w')
+            sys.stdout = open("../data/mergesort.csv", 'w')
         case "h":
             func = hsort
-            sys.stdout = open("../data/heapsort", 'w')
+            sys.stdout = open("../data/heapsort.csv", 'w')
         case "i":
             func = isort
-            sys.stdout = open("../data/insertionsort", 'w')
+            sys.stdout = open("../data/insertionsort.csv", 'w')
         case "b":
             func = partial(bsort)
-            sys.stdout = open("../data/bucketsort", 'w')
+            sys.stdout = open("../data/bucketsort.csv", 'w')
         case _:
             usage()
             exit(0)
-
-    for i in range(100, 10000):
+    # headers for csv files
+    print("input_size,comp,swap,basic,time,mem")
+    for i in range(100, 10000, 100):
         del arr
         arr = [random() for _ in range(i)]
         measure(arr)
--- data/plot.py
+++ data/plot.py
@@ -7,19 +7,19 @@ def usage():
     print("Usage: plot FILE")
 
 def plot(df, key, label):
-    file = sys.argv[1].split()[0]
-    file += key + ".png"
+    basename = sys.argv[1].split(".")[0]
+    filepath = f"plots/{basename}/{basename}_{key}.png"
     x = df['input_size']
     y = df[key]
 
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(20,20))
     plt.xlabel("Input Size", fontsize=20, fontname="monospace")
-    plt.ylabel(lable, fontsize=20, fontname="monospace")
+    plt.ylabel(label, fontsize=20, fontname="monospace")
     plt.xticks(fontsize=18)
     plt.yticks(fontsize=18)
 
     plt.plot(x, y, color="green")
-    plt.savefig(file)
+    plt.savefig(filepath)
 
 
 if __name__ == "__main__":
