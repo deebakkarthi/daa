78c6ab4b58666faf617573ca2f73511b90d1cbf6
Deebakkarthi C R <dbk@deebakkarthi.com>
2022-11-06 23:05:40 +0530

    Added correct topo() and plot

--- graph/dna.py
+++ graph/dna.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import random
 from collections import defaultdict
-from collections import deque
+import sys
+import matplotlib.pyplot as plt
 
 class uu_graph:
     def __init__(self):
         self.map = defaultdict(lambda :{"left":[],"right":[]})
+    def __len__(self):
+        return len(self.map)
     def graph_construct(self, edgelist):
         for i in edgelist:
             (u, v, d) = i
@@ -23,35 +26,42 @@ class uu_graph:
         for i in self.map:
             yield i
 
-    def topo(self):
-        indegree = dict()
-        # Queue of vertices with indegree of 0
-        ready = deque()
-        order = list()
-        for i in self:
-            indegree[i] = len(self.map[i]["left"])
-            if indegree[i] == 0:
-                ready.append(i)
-        while ready:
-            tmp = ready.popleft()
-            order.append(tmp)
-            for i in self.map[tmp]["right"]:
-                # Reducing the dependency as tmp is added to the order. So it's
-                # neighbours are free to enter the order as well
-                indegree[i] -= 1
-                # If it doesn't have any other dependencies add to ready queue
-                if indegree[i] == 0:
-                    ready.append(i)
-        return order
+def topo(graph):
+    indegree = dict()
+    ready = list()
+    vis = dict()
+    path = list()
+    sequences = list()
+    for i in graph:
+        vis[i] = False
+        indegree[i] = len(graph.map[i]["left"])
+    topo_rec(graph, indegree, vis, path, sequences)
+    return sequences
 
 
-def dna_gen(k):
+def topo_rec(graph, indegree, vis, path, sequences):
+    for i in graph:
+        if indegree[i] == 0 and not vis[i]:
+            for j in graph.map[i]["right"]:
+                indegree[j] -= 1
+            path.append(i)
+            vis[i] = True
+            topo_rec(graph, indegree, vis, path, sequences)
+            for j in graph.map[i]["right"]:
+                indegree[j] += 1
+            path.pop()
+            vis[i] = False
+    if len(path) == len(graph):
+        sequences.append(path.copy())
+
+
+def dna_gen(num_frags, num_clues):
     DIRS = ["l","r"]
-    og = [frag_gen(random.randint(1,2)) for _ in range(k)]
+    og = [frag_gen(random.randint(5,6)) for _ in range(num_frags)]
     og = [*set(og)]
     fragments = set()
     # Take k pair from og and add the directional detail to fragments
-    for _ in range(2*k):
+    for _ in range(num_clues):
         fi = random.randint(0,len(og)-1)
         fj = random.randint(0,len(og)-1)
         if fi < fj:
@@ -68,13 +78,30 @@ def frag_gen(k):
     BASES = ["a","c","g","t"]
     return "".join(random.choice(BASES) for _ in range(k))
 
+def usage():
+    print("Usage: dna NUM_FRAGS")
+    exit()
+
 if __name__ == "__main__":
-    fragments, og = dna_gen(10)
-    for i in fragments:
-        print(i)
-    g = uu_graph()
-    g.graph_construct(fragments)
-    for i in g:
-        print(i, g.map[i])
-    print(f"Original sequence: {og}")
-    print(g.topo())
+    if len(sys.argv) < 2:
+        usage()
+    num_frags = int(sys.argv[1])
+    x = []
+    y = []
+    for i in range(num_frags, num_frags*num_frags):
+        fragments, og = dna_gen(num_frags, i)
+        g = uu_graph()
+        g.graph_construct(fragments)
+        #for i in g:
+        #    print(i,g.map[i])
+        #print(f"OG:{og}")
+        tmp = topo(g)
+        print(i,len(tmp))
+        x.append(i)
+        y.append(len(tmp))
+    plt.plot(x,y,label=f"#(fragments) = {num_frags}")
+    plt.yscale("log")
+    plt.xlabel("Number of clues")
+    plt.ylabel("Number of possible sequences(log)")
+    plt.legend()
+    plt.show()
